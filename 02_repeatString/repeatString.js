7eb7772096aff1ab2632d172b4f850a663c3be26
Flavio Alves <flavioasjr@gmail.com>
2022-03-02 16:13:26 -0300

    feito os exercícios do 1 ao 7.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
 
+    if (string.length === 0) return '';
+
+    let newString = '';
+
+    for (i = 0; i < num; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let array = Array.from(arguments[0]);
+    // console.log(array);
+    let newArray = [];
+    let check = false;
 
+    for (let index = 0; index < array.length; index++) {
+        for (let i = 1; i < arguments.length; i++) {
+            if (array[index] === arguments[i]) {
+                array.splice(index, 1);
+                index--;
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
